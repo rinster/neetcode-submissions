ce03af8346ebe53f3d85ed3f43a0e9e09a62d2d2
Erine Tieu <enatnat.08@gmail.com>
2026-08-23 20:10:11 -0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        # Solution 1: Brute Naive O(n log n) | O(1)
+        #return sorted(s) == sorted(t)
+
+        # Solution 2: Hash O(n + m) | O(n)
+        if len(s) != len(t):
+            return False
+        countT, countS = {}, {}
+
+        for i in range(len(s)):
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+
+        return countS == countT
+
+        # Solution 3: Hash Table using Array O(n + m) | O(n)
+
+
+        
