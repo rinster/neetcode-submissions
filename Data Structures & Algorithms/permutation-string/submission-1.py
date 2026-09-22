5c398229633d523229a3d1368d3d5d3546d03d63
Erine Tieu <enatnat.08@gmail.com>
2026-09-22 12:37:23 -0700

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        n1 = len(s1)
+        n2 = len(s2)
+        
+        if n1 > n2: return False
+
+        s1Count = [0] * 26
+        s2Count = [0] * 26
+        
+        # initial values for first set of window
+        for i in range(n1):
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
+
+        for i in range(n2-n1):               # n2-n1 is the number of slides
+            if s1Count == s2Count:
+                return True
+            
+            # reduce leaving char count
+            s2Count[ord(s2[i]) - ord('a')] -= 1
+            
+            # increase introduced char count
+            s2Count[ord(s2[i+n1]) - ord('a')] += 1
+
+        return s1Count == s2Count  
+
+        
